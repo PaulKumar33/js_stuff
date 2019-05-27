45b44c8f6a788c84b9043b4bed19e2633a6b98ce
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2019-05-27 18:41:31 -0400

    committing a new script

--- groceryList.js
+++ groceryList.js
@@ -12,8 +12,8 @@ function ProduceObject(name, quantity, dateBought, bestBefore){
     groceryObject = {
         name: fName,
         quant: fQuantity,
-        dateb: fDateBought;
-        bestb: fBestBefore;
+        dateb: fDateBought,
+        bestb: fBestBefore,
     }
 
     return groceryObject;
@@ -30,13 +30,48 @@ function DisplayInfo(obj){
 //everything that is present. SHould be an array of objects
 
 //example ar = [obj1, obj2, obj3, ...... ,objn]
-var groceryArray = [];
+/* further note the following:
+
+obj has the following structure
+{
+    name: "some name",
+    quantity: "some quantity",
+    date bought: "some date",
+    best before: "some date"
+}
+
+the elements in this array show correspond to the key
+*/
+var groceryArray = new Array();
 
 function PopulateMainArray(newItem){
     //this method is the main  population method
-
+    groceryArray.push(newItem)
 }
 
+function ParseMainArray(arrayName){
+    var mainAr = groceryArray;
+    for(let element in groceryArray){
+        let el =  groceryArray[element];
+        if(el.name == arrayName){
+            //flow for accessing the element
+            console.log("Yes this exists!")
 
+        }
+    }
+}
 
 //main block
+
+var testObject = new ProduceObject("carrot",
+2, 'today', 'tomorrow');
+var testObject2 = new ProduceObject("brocoli",
+1, 'today', 'tomorrow')
+console.log(testObject);
+PopulateMainArray(testObject);
+PopulateMainArray(testObject2);
+console.log("this is a test")
+console.log(groceryArray[1].name);
+ParseMainArray('carrot');
+
+
