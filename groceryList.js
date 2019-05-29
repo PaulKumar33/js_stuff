3c452171ca3879227554b31c29f3cdcbbab46eeb
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2019-05-29 19:16:40 -0400

    committing changes

--- groceryList.js
+++ groceryList.js
@@ -12,8 +12,18 @@ function ProduceObject(name, quantity, dateBought, bestBefore){
     groceryObject = {
         name: fName,
         quant: fQuantity,
-        dateb: fDateBought,
-        bestb: fBestBefore,
+
+        dateb: {
+            year: dateBought.year,
+            month: dateBought.month,
+            day: dateBought.day
+        },
+
+        bestb: {
+            year: dateBought.year,
+            month: dateBought.month,
+            day: dateBought.day
+        }
     }
 
     return groceryObject;
@@ -61,17 +71,74 @@ function ParseMainArray(arrayName){
     }
 }
 
-//main block
+function DateConstructor(datePosition){
+    /*
+    if datePosition == 0 -> get the current date
+    if datePosition == 1 -> construct a later date
+    if datePosition == 2 -> get current time within end date and start date
+    */
+   if(datePosition == 0){
+       var todaysDate = new Date();
+       var returnDate = todaysDate.getFullYear()+"/"+todaysDate.getMonth()+"/"+todaysDate.getDay()
+       return todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDay();
+   }
+}
 
+function StringConstructor(headerLength, stringEntry){
+    //this function builds a string line to be displayed
+    var stringLength = stringEntry.length;
+    if(headerLength > stringLength){
+        //this needs to be satisphied
+        var lengthDifference = headerLength - stringLength;
+        if(lengthDifference % 2 != 0){
+            //there is a remainder
+            var endLengths = (lengthDifference-1)/2;
+
+            var endLeft= endLengths;
+            var endRight = endLengths+1;
+            
+            var leftString = ""+"=".repeat(endLeft-1)+ " ";
+            var rightStirng = " "+"=".repeat(endRight-1);            
+        }
+        else if(lengthDifference%2==0){
+            //this block is for even numbers
+            var endLengths = lengthDifference/2;
+            var leftString = ""+"=".repeat(endLengths-1)+" ";
+            var rightStirng = " "+"=".repeat(endLengths-1);
+        }
+        var constructedString = leftString+stringEntry+rightStirng;
+        return constructedString;
+    }
+}
+
+function DisplayAllElements(){
+    //this method displays all the groceries by name,
+    //can access them after
+    //later add functionality for order based on expiry
+    //or order on most use (involve ML!!)
+    //var names = []
+    var lengthVal = "=================".length
+    console.log("=================");
+    console.log("=Available items=");
+    for(var index = 0; index < groceryArray.length; index ++){
+        console.log(StringConstructor(lengthVal,groceryArray[index].name));
+    }
+    console.log("=================");
+}
+
+//main block
+var year_, month_, day_ = DateConstructor(1);
 var testObject = new ProduceObject("carrot",
-2, 'today', 'tomorrow');
+2, {year: year_, month: month_, day:day_}, {year: year_, month: month_, day:day_});
 var testObject2 = new ProduceObject("brocoli",
-1, 'today', 'tomorrow')
+1, {year: year_, month: month_, day:day_}, {year: year_, month: month_, day:day_})
 console.log(testObject);
 PopulateMainArray(testObject);
 PopulateMainArray(testObject2);
 console.log("this is a test")
 console.log(groceryArray[1].name);
 ParseMainArray('carrot');
+
+DisplayAllElements();
 
 
